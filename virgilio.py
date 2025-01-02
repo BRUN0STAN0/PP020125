1d63b18ac2118f33b336f31a7460fc9639f851d8
PACiN00B <contact@brunostano.comm>
2025-01-02 20:53:22 +0100

    Esercizio 8 Completato

--- virgilio.py
+++ virgilio.py
@@ -62,6 +62,30 @@ class Virgilio:
         verses = self.__readlines_canto__(canto_number)
         return max(verses, key=len)
 
+    def get_longest_canto(self):
+
+        files_path = os.listdir(self.directory)
+
+        canti = []
+        for file in files_path:
+            canto_number = file.split("_")[1].split(".")[0]
+            verse_list = len(self.__readlines_canto__(canto_number))
+            canto = {
+                "canto_number": int(canto_number),
+                "canto_len": verse_list
+            }
+            canti.append(canto)
+
+        longest_canto = {
+            "canto_number": 0,
+            "canto_len": 0
+        }
+        for canto in canti:
+            if canto["canto_len"] > longest_canto["canto_len"]:
+                longest_canto["canto_number"] = canto["canto_number"]
+                longest_canto["canto_len"] = canto["canto_len"]
+        return longest_canto
+
 
 def main():
     """This function allows you to call and execute the main program in an external module."""
@@ -72,35 +96,39 @@ def main():
         canto_to_read = input("\nPerfavore, fornisci un valido numero di Canto.\n\nCanto Numero: ")
         try:
             int(canto_to_read)
-            # Exercise 1
-            canto = reader.read_canto_lines(canto_to_read)
-            print(f"\n{canto} \n")
 
-            # Exercise 2
-            verses_of_canto = reader.count_verses(canto_to_read)
-            print(f"Versi del canto: {verses_of_canto}")
+            # # Exercise 1
+            # canto = reader.read_canto_lines(canto_to_read)
+            # print(f"\n{canto} \n")
 
-            # Exercise 3
-            tercets_of_canto = reader.count_tercets(canto_to_read)
-            print(f"Terzine del canto: {tercets_of_canto}\n")
+            # # Exercise 2
+            # verses_of_canto = reader.count_verses(canto_to_read)
+            # print(f"Versi del canto: {verses_of_canto}")
 
-            # Exercise 4
-            word = input("Fornisci una parola, per contare l'utilizzo: ")
-            recurrence_word = reader.count_word(canto_to_read, word)
-            print(f"\nNumero di utilizzo della parola '{word}' nel {canto_to_read}: {recurrence_word}\n")
+            # # Exercise 3
+            # tercets_of_canto = reader.count_tercets(canto_to_read)
+            # print(f"Terzine del canto: {tercets_of_canto}\n")
 
-            # Exercise 5
-            verse_with_word = reader.get_verse_with_word(canto_to_read, word)
-            print(f"Primo utilizzo della parola '{word}': {verse_with_word}")
+            # # Exercise 4
+            # word = input("Fornisci una parola, per contare l'utilizzo: ")
+            # recurrence_word = reader.count_word(canto_to_read, word)
+            # print(f"\nNumero di utilizzo della parola '{word}' nel {canto_to_read}: {recurrence_word}\n")
 
-            # Exercise 6
-            verses_with_word = reader.get_verses_with_word(canto_to_read, word)
-            print(f"Tutti gli utilizzi della parola '{word}: {verses_with_word}\n")
+            # # Exercise 5
+            # verse_with_word = reader.get_verse_with_word(canto_to_read, word)
+            # print(f"Primo utilizzo della parola '{word}': {verse_with_word}")
 
-            # Exercise 6
-            longest_verse = reader.get_longest_verse(canto_to_read)
-            print(f"Verso piu lungo del canto: {longest_verse}\n")
+            # # Exercise 6
+            # verses_with_word = reader.get_verses_with_word(canto_to_read, word)
+            # print(f"Tutti gli utilizzi della parola '{word}: {verses_with_word}\n")
 
+            # # Exercise 6
+            # longest_verse = reader.get_longest_verse(canto_to_read)
+            # print(f"Verso piu lungo del canto: {longest_verse}\n")
+
+            # Exercise 7
+            longest_canto = reader.get_longest_canto()
+            print(f"The longest canto is: {longest_canto}")
             break
         except Exception as err:
             print(f"{type(err).__name__}: {err}")
