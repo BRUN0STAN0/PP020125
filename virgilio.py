7c940172a06277086a23f74fb63af13963d2e7d9
PACiN00B <contact@brunostano.comm>
2025-01-02 18:52:38 +0100

    Esercizio 2 Completato

--- virgilio.py
+++ virgilio.py
@@ -5,21 +5,29 @@ class Virgilio:
     def __init__(self, directory: str):
         self.directory = directory
 
-    def __file_path_exist(self, canto_number: int):
+    def __file_path_exist__(self, canto_number: int):
         file_path = os.path.join(self.directory, f"Canto_{canto_number}.txt")
-        file_path_exist = os.path.exists(file_path)
-        if not file_path_exist:
+        if not os.path.exists(file_path):
             raise FileNotFoundError(f"Il canto numero {canto_number} non esiste.")
         return file_path
 
-    def read_canto_lines(self, canto_number: int):
-
-        file_path = self.__file_path_exist(canto_number)
+    def __readlines_canto__(self, canto_number):
+        file_path = self.__file_path_exist__(canto_number)
         with open(file_path, "r", encoding="utf-8") as file:
-            lines = file.read()
+            lines = file.readlines()
         return lines
 
+    def read_canto_lines(self, canto_number: int):
+        lines = self.__readlines_canto__(canto_number)
+        formatted_lines = "".join(lines)
+        return formatted_lines
+
     def count_verses(self, canto_number: int):
+        list_verses = self.__readlines_canto__(canto_number)
+        number_of_verses = len(list_verses)
+        return number_of_verses
+
+    def count_tercets(self, canto_number: int):
         return
 
 
@@ -34,6 +42,7 @@ def main():
             int(canto_to_read)
             canto = reader.read_canto_lines(canto_to_read)
             print(f"\n{canto}")
+            print(reader.count_verses(canto_to_read))
             break
         except Exception as err:
             print(f"{type(err).__name__}: {err}")
